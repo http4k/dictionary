73cd1cc8e84e298ea8ac21ccc85d96956514d5df
isanchez <ivan@http4k.org>
2024-10-11 19:15:13 +0100

    Add dictionary code

--- dictionary-function/src/main/kotlin/org/http4k/dictionary/DictionaryApp.kt
+++ dictionary-function/src/main/kotlin/org/http4k/dictionary/DictionaryApp.kt
@@ -1,9 +1,11 @@
 package org.http4k.dictionary
 
 import org.http4k.client.JavaHttpClient
+import org.http4k.core.Method
 import org.http4k.core.Method.GET
 import org.http4k.core.Request
 import org.http4k.core.Response
+import org.http4k.core.Status
 import org.http4k.core.Status.Companion.OK
 import org.http4k.routing.bind
 import org.http4k.routing.path
@@ -11,15 +13,11 @@ import org.http4k.routing.routes
 import org.http4k.serverless.ApiGatewayV2LambdaFunction
 
 val http4kApp = routes(
-    "/echo/{message:.*}" bind GET to {
-        Response(OK).body(
-            it.path("message") ?: "(nothing to echo, use /echo/<message>)"
-        )
+    "/{word}" bind GET to { request: Request ->
+        val words = setOf("the", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog", "cat")
+        if (request.path("word") in words) Response(OK) else Response(Status.NOT_FOUND)
     },
-    "/external" bind GET to {
-        Response(OK).body(JavaHttpClient()(Request(GET, "http://httpbin.org/get")).bodyString())
-    },
-    "/" bind GET to { Response(OK).body("Hello from Lambda URL!").header("content-type", "text/html; charset=utf-8") }
+    "/" bind GET to { Response(OK).body("Welcome to the http4k Dictionary app!").header("content-type", "text/html; charset=utf-8") }
 )
 
 @Suppress("unused")
